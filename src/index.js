c38541a0cf59b3c4fa63a76a2f6ce8bbbc297a31
wushukms <safina1999@rambler.ru>
2018-09-12 01:24:58 +0300

    crazysorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.arr = [];
+        this.criteria = (a, b) => a - b;
+        // your implementation
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.arr.push(element);
+        // your implementation
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.arr[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.arr.length;
+        // your implementation
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.arr.valueOf();
+        // your implementation
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        let elements_number = indices.length;
+        let arr2 = [];
+        for (let i = 0 ; i < elements_number; i++){
+            arr2.push(this.arr[indices[i]]);
+            //this.arr[elements_number[i]]
+        }
+        indices.sort((a, b) => a - b);
+        arr2.sort(this.criteria);
+        for (let i = 0 ; i < elements_number; i++){
+            this.arr[indices[i]]=arr2[i];
+        }
+        // your implementation
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        this.criteria = compareFunction;
+        // your implementation
+    }
 }
 
 module.exports = Sorter;
